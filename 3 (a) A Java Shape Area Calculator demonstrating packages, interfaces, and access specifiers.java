562a1f8a35328fbb040b697fae4881eb7bb96717
hasi-sea <haashimcps147@gmail.com>
2026-07-25 10:45:58 +0530

    Create 3 (a) A Java Shape Area Calculator demonstrating packages, interfaces, and access specifiers.java

---
+++ 3 (a) A Java Shape Area Calculator demonstrating packages, interfaces, and access specifiers.java
@@ -0,0 +1,55 @@
+interface Shape {
+    double calculateArea();
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class Triangle implements Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(7.0);
+        Shape rectangle = new Rectangle(10.0, 5.0);
+        Shape triangle = new Triangle(8.0, 6.0);
+
+        System.out.println("Area of Circle: " + circle.calculateArea());
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+        System.out.println("Area of Triangle: " + triangle.calculateArea());
+    }
+}
